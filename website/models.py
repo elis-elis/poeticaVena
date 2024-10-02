70da6277055d1f1a79f070ca8341e76f129c30eb
elis-elis <elisprostotak@gmail.com>
2024-10-02 23:29:12 +0200

    add PoemType class and fix other classes in models.py

--- website/models.py
+++ website/models.py
@@ -1,27 +1,35 @@
 from . import db
-from flask_sqlalchemy import SQLAlchemy
 from flask_login import UserMixin
 from sqlalchemy.sql import func
 
 
-class Users(db.Model, UserMixin):
-    __tablename__ = 'users'
+class Poet(db.Model, UserMixin):
+    __tablename__ = 'poets'
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Colum(db.String(50), nullable=False, unique=True)
+    poet_name = db.Column(db.String(50), nullable=False, unique=True)
     email = db.Column(db.String(100), nullable=False, unique=True)
     password_hash = db.Column(db.String(260), nullable=False)
-    poems = db._relationship('Poems', backref='author', lazy=True)
+    poems = db.relationship('Poem', backref='poet', lazy=True)
     created_at = db.Column(db.DateTime(timezone=True), default=func.now())
 
-class Poems(db.Model):
+class Poem(db.Model):
     __tablename__ = 'poems'
 
     id = db.Column(db.Integer, primary_key=True)
-    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
-    poem_type_id = db.Column(db.Integer, db.ForeignKey('poem_types.id'), nullable=False)
+    poets_id = db.Column(db.Integer, db.ForeignKey('poet.id'), nullable=False)
+    poem_type_id = db.Column(db.Integer, db.ForeignKey('poem_type.id'), nullable=False)
     title = db.Column(db.String(255), nullable=False)
     is_collaborative = db.Column(db.Boolean, default=False)
     is_published = db.Column(db.Boolean, default=False)
     created_at = db.Column(db.DateTime(timezone=True), default=func.now())
-    updated_at = db.Column(db.DateTime(timezone=True), default=func.now())
+    updated_at = db.Column(db.DateTime(timezone=True), onupdate=func.now())
+
+class PoemType(db.Model):
+    __tablename__ = 'poem_types'
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+    description = db.Column(db.Text, nullable=False)
+    criteria = db.Column(db.Text, nullable=False)
+    poems = db.relationship('Poem', backref='poem_type', lazy=True)
