bbb9e4bd536345b346dd21f337baf29765f4dee7
elis-elis <elisprostotak@gmail.com>
2024-10-02 20:56:26 +0200

    add Poems class in models.py, delete fucntions in auth.py

--- website/auth.py
+++ website/auth.py
@@ -1,10 +1,11 @@
 from flask import Blueprint, render_template, request, flash, redirect, url_for
 from . import db
-from flask_login import login_user, login_required, logout_user, current_user
 from werkzeug.security import generate_password_hash, check_password_hash
+from flask_jwt_extended import create_access_token
 
 
 auth = Blueprint('auth', __name__)
+
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -13,13 +14,4 @@ def login():
 @auth.route('register', methods=['GET', 'POST'])
 def register():
     return render_template('register.html')
-
-
-
-# Optional helper methods for password management
-def set_password(self, password):
-    self.password_hash = generate_password_hash(password, method='sha256')
-
-def check_password(self, password):
-    return check_password_hash(self.password_hash, password)
-    
+  
--- website/models.py
+++ website/models.py
@@ -5,14 +5,23 @@ from sqlalchemy.sql import func
 
 
 class Users(db.Model, UserMixin):
+    __tablename__ = 'users'
+
     id = db.Column(db.Integer, primary_key=True)
     username = db.Colum(db.String(50), nullable=False, unique=True)
     email = db.Column(db.String(100), nullable=False, unique=True)
     password_hash = db.Column(db.String(260), nullable=False)
-    poems = db._relationship('Poems')
-    created_at = db.Column(db.DateTime(timezone=True), default=db.func.now())
+    poems = db._relationship('Poems', backref='author', lazy=True)
+    created_at = db.Column(db.DateTime(timezone=True), default=func.now())
 
 class Poems(db.Model):
+    __tablename__ = 'poems'
+
     id = db.Column(db.Integer, primary_key=True)
-
-
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
+    poem_type_id = db.Column(db.Integer, db.ForeignKey('poem_types.id'), nullable=False)
+    title = db.Column(db.String(255), nullable=False)
+    is_collaborative = db.Column(db.Boolean, default=False)
+    is_published = db.Column(db.Boolean, default=False)
+    created_at = db.Column(db.DateTime(timezone=True), default=func.now())
+    updated_at = db.Column(db.DateTime(timezone=True), default=func.now())
