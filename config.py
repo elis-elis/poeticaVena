a78151498187bbcfbfac8b4333c97fdd0772d866
elis-elis <elisprostotak@gmail.com>
2024-11-19 12:42:10 +0100

    change to db_url in config.py

--- config.py
+++ config.py
@@ -5,7 +5,5 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY', 'one_more_secret_key')
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL_SERVER')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
-
-print("DATABASE_URL:", os.environ.get('DATABASE_URL'))
